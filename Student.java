c628280c4fb157224066704d4318c858254151cb
ChanAn200 <125016327+ChanAn200@users.noreply.github.com>
2023-05-12 10:18:52 +0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,50 @@
+package advance.dev;
+
+public class Student {
+     String name;
+     int age;
+     String address;
+     int phone;
+     double dtb;
+     
+     public Student(String name, int age, String address, int phone, double dtb) {
+    	 super();
+    	 this.name = name;
+    	 this.age = age;
+    	 this.address = address;
+    	 this.phone = phone;
+    	 this.dtb = dtb;
+     }
+     
+     public String getName() {
+    	 return name;
+     }
+     
+     public void setName(String name) {
+    	 this.name = name;
+     }
+     
+     public int getAge() {
+    	 return age;
+     }
+     
+     public void setAge(int age) {
+    	 this.age = age;
+     }
+     
+     public String getAddress() {
+    	 return address;
+     }
+     
+     public void setAddress(String address) {
+    	 this.phone = phone;
+     }
+     
+     public double getDtb() {
+    	 return dtb;
+     }
+     
+     public void setDtb(double dtb) {
+    	 this.dtb = dtb;
+     }
+}
